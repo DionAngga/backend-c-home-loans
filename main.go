37cce4b3d08201fece6f8cbc15991c837335bddc
7-Rohan <mrizkim18@gmail.com>
2021-10-21 21:57:17 +0700

    fix CORS

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"net/http"
+	"os"
+
 	"github.com/rysmaadit/go-template/app"
 	"github.com/rysmaadit/go-template/cli"
-	"os"
 )
 
 func main() {
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
+	})
 	c := cli.NewCli(os.Args)
 	c.Run(app.Init())
 }
